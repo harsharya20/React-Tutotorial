31cd60e29b04df593334d19ca5884994ec50f8b8
Harsh <harsharya7002@gmail.com>
2022-03-22 19:15:14 +0530

    show add task state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import AddTask from "./components/AddTask";
 
 
 const App = () => {
+  const [showAddTask , setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -47,10 +48,10 @@ const App = () => {
 
   return (
     <div className="container">
-    <Header />
-    <AddTask onAdd = {addTask} />
+    <Header onAdd={() => setShowAddTask(!showAddTask)} />
+    {showAddTask && <AddTask onAdd = {addTask} />}
     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder} />) :(
-      'No tasks to display'
+      'No tasks to display....'
     )}
     </div>
   );
